827818428dfdccd8889a12840d80f3780e6ed5b7
chunkpig <49008602+chunkpig@users.noreply.github.com>
2022-09-09 16:44:57 +0800

    Add files via upload

---
+++ docs/static/home.9627d9bf.js
@@ -0,0 +1,1 @@
+import{D as s}from"./search.2bd8d741.js";function e(t){return s.get("/playlist/catlist",t)}function i(t){return s.get("/top/playlist",t)}function l(t){return s.get("/user/playlist",t)}export{i as a,l as b,e as g};
