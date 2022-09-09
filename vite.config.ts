b8cb86871a00ce7621275e6c66b2dc137df14a28
chunkpig <zhunanqi@youzidata.com>
2022-09-09 16:05:48 +0800

    feat-更新

--- vite.config.ts
+++ vite.config.ts
@@ -92,9 +92,9 @@ export default defineConfig({
     //浏览器兼容性  "esnext"|"modules"
     target: 'modules',
     //指定输出路径
-    outDir: 'dist',
+    outDir: 'docs',
     //生成静态资源的存放路径
-    assetsDir: 'assets',
+    assetsDir: 'static',
     //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
     assetsInlineLimit: 4096,
     //启用/禁用 CSS 代码拆分
